6f1e8f7cbaaada4c66c597118378a7344d8a8d34
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2025-02-13 14:36:24 +0530

    Updated

--- script/script.js
+++ script/script.js
@@ -1,2 +1,15 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logo-slider").appendChild(copy);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("change", function() {
+        if (this.checked) {
+            menu.style.display = "flex";
+        } else {
+            menu.style.display = "none";
+        }
+    });
+});
